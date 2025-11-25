a35f8ac7e41c9666f3f82049ce08bc93e0f238bd
Jose Garrera <josegarrera@gmail.com>
2025-11-25 15:30:39 -0300

    green: add tests for email and domain uniqueness validation in addUser use case

--- src/useCases/addUser.test.ts
+++ src/useCases/addUser.test.ts
@@ -20,4 +20,31 @@ describe('Add user', () => {
     const users = await usersRepository.getAll();
     expect(users).toEqual([user]);
   });
+
+  it('should not add a user if the email is already in use', async () => {
+    const alreadyExistingUser = User.create({
+      name: 'John Doe',
+      email: Email.create('email@dominio.com'),
+      password: Password.create('aaaaaa123'),
+    });
+    const usersRepository = new InMemoryUsersRepository([alreadyExistingUser]);
+    const addUser = new AddUser(usersRepository);
+    await expect(async () => addUser.execute(alreadyExistingUser)).rejects.toThrow('Email already in use');
+  });
+
+  it('should not add a user if the domain is already in use', async () => {
+    const alreadyExistingUser = User.create({
+      name: 'John Doe',
+      email: Email.create('email1@dominio.com'),
+      password: Password.create('aaaaaa123'),
+    });
+    const usersRepository = new InMemoryUsersRepository([alreadyExistingUser]);
+    const newUser = User.create({
+      name: 'John Doe',
+      email: Email.create('email2@dominio.com'),
+      password: Password.create('aaaaaa123'),
+    });
+    const addUser = new AddUser(usersRepository);
+    await expect(async () => addUser.execute(newUser)).rejects.toThrow('Domain already in use');
+  });
 });
