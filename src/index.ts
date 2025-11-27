040c5918d4fe6f137522134eeb99b45b49e8a82b
Jose Garrera <josegarrera@gmail.com>
2025-11-27 15:16:52 -0300

    feat: add FileUsersRepository with configurable repo via --repo flag

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import { View } from './view/view';
-import { InMemoryUsersRepository } from './useCases/adapters/inMemoryUsersRepository';
+import { createUsersRepository, getRepositoryTypeFromArgs } from './useCases/adapters/repositoryFactory';
 import { ListUsers } from './useCases/listUsers';
 import { AddUser } from './useCases/addUser';
 import { Presenter } from './presenter/presenter';
 
-const usersRepository = new InMemoryUsersRepository();
+const repoType = getRepositoryTypeFromArgs();
+console.log(`Using ${repoType} repository`);
+
+const usersRepository = createUsersRepository(repoType);
 const listUser = new ListUsers(usersRepository);
 const addUser = new AddUser(usersRepository);
 
